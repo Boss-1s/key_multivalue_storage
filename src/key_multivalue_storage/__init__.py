b25329664477187cc0bb34c0bd33f03ef7ad5324
Boss_1s <95505913+Boss-1s@users.noreply.github.com>
2026-05-13 22:26:44 +0000

    setup env - let's get this damn thing done!

---
+++ src/key_multivalue_storage/__init__.py
@@ -0,0 +1,9 @@
+__version__ = "1.2.2.20260506.2"
+__version_internal__ = "kms-v1.2.2/2026.05.06b"
+__author__ = "Boss_1s"
+__email__ = ""
+__license__ = "GPLv2"
+
+from .key_multivalue_storage import Storage
+
+__all__ = ["Storage"]
