44758a3c5e6681bb35c5bf77ad0bf0ad32156206
Boss_1s <95505913+Boss-1s@users.noreply.github.com>
2026-05-13 22:57:21 +0000

    fix: unspecified email

---
+++ README.md
@@ -0,0 +1,3 @@
+omg its my readme....
+
+ill get this done at some point trust
--- src/key_multivalue_storage/__init__.py
+++ src/key_multivalue_storage/__init__.py
@@ -1,7 +1,7 @@
 __version__ = "1.2.2.20260506.2"
 __version_internal__ = "kms-v1.2.2/2026.05.06b"
 __author__ = "Boss_1s"
-__email__ = ""
+__email__ = "95505913+Boss-1s@users.noreply.github.com"
 __license__ = "GPLv2"
 
 from .key_multivalue_storage import Storage
