293d1e934878447549f49f24e1e2a4c5c367aed0
Boss_1s <95505913+Boss-1s@users.noreply.github.com>
2026-05-15 20:49:07 -0500

    Update __init__.py

--- src/key_multivalue_storage/__init__.py
+++ src/key_multivalue_storage/__init__.py
@@ -1,5 +1,5 @@
-__version__ = "1.2.2.20260506.2"
-__version_internal__ = "kms-v1.2.2/2026.05.06b"
+__version__ = "1.2.2.20260515"
+__version_internal__ = "kms-v1.2.2/2026.05.15"
 __author__ = "Boss_1s"
 __email__ = "95505913+Boss-1s@users.noreply.github.com"
 __license__ = "GPLv2"
