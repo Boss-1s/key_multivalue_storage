24dcf13eb060bec81f091a1bf82ffd8703523062
Boss_1s <95505913+Boss-1s@users.noreply.github.com>
2026-05-21 11:34:00 -0500

    fix: let's follow the readme
    - namespaces 'kms' and 'key-multivalue-storage' added to abide by the docs and prepare for kms-semver2.0
    - added PendingDeprecationWarning for the namespace 'kms' switch in kms-semver2.0
    
    og comment: https://github.com/Boss-1s/key_multivalue_storage/pull/4#issuecomment-4504347177
    
    Signed-off-by: Boss_1s <95505913+Boss-1s@users.noreply.github.com>

--- src/key_multivalue_storage/__init__.py
+++ src/key_multivalue_storage/__init__.py
@@ -4,6 +4,18 @@ __author__ = "Boss_1s"
 __email__ = "95505913+Boss-1s@users.noreply.github.com"
 __license__ = "GPLv2"
 
+from . import key_multivalue_storage as kms
+from . import key_multivalue_storage
+from . import key_multivalue_storage as key-multivalue-storage
 from .key_multivalue_storage import Storage
+import warnings
+
+warnings.warn("Going from kms-semver2.0, the module"+
+              "names 'key_multivalue_storage' and"+
+              "'key-multivalue-storage' will be deprecated"+
+              "in favor of the name 'kms'. Please note this"+
+              "change accordingly, thank you!",
+              PendingDeprecationWarning
+             )
 
 __all__ = ["Storage"]
