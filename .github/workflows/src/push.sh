f040b8ace633ab69853ea68783f0437357a42cc9
Boss_1s <95505913+Boss-1s@users.noreply.github.com>
2026-05-24 11:50:01 -0500

    fix: switch branch and pray it works
    Signed-off-by: Boss_1s <95505913+Boss-1s@users.noreply.github.com>

--- .github/workflows/src/push.sh
+++ .github/workflows/src/push.sh
@@ -7,15 +7,19 @@ GIT_STATUS_OUTPUT=$(git status --porcelain)
 
 # Check if the output is empty
 if [ -z "$GIT_STATUS_OUTPUT" ]; then
-  echo "Git working tree is clean (no uncommitted changes)."
+  echo 'Git working tree is clean (no uncommitted changes).'
   exit 0
 else
-  echo "Git working tree has uncommitted changes:"
+  echo 'Git working tree has uncommitted changes:'
   echo "$GIT_STATUS_OUTPUT"
   echo "These changes will be pushed to the $BRANCH branch."
 fi
 
+git switch $BRANCH
 git add .
-git restore --staged .github/.tmp/
+git restore --staged .github/.tmp
+echo 'Unstaged all commits in .github/.tmp/.'
+echo 'New Git Status:'
+echo "$GIT_STATUS_OUTPUT"
 git commit -m "[Release] Update file version"
 git push origin $BRANCH
