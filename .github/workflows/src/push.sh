e988c85f84cb3a3039b7bd6a7e73d9eacdcd1c88
Boss_1s <95505913+Boss-1s@users.noreply.github.com>
2026-06-09 12:58:17 -0500

    chore: update commit msg and desc
    Signed-off-by: Boss_1s <95505913+Boss-1s@users.noreply.github.com>
    (cherry picked from commit 6f2903ce1fc83f6fd1a5842a78c1dbc277998a39)
    Signed-off-by: GitHub <noreply@github.com>

--- .github/workflows/src/push.sh
+++ .github/workflows/src/push.sh
@@ -18,7 +18,7 @@ if [ -z "$GIT_STATUS_OUTPUT" ]; then
 fi
 
 git add .
-git commit -m "[Release] Update file version"
+git commit -m "release: version update [skip-ci]" -m "This commit is standard release procedure and was automatically commited as part of a release. This commit was created and pushed by automation."
 
 if [[ "$DUMMY" == "true" ]]; then
   git push origin "$BRANCH" --dry-run
