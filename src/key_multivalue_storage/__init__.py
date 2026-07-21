8cb862d2f0676b3ab6cf54754a0514db3c0cb004
Boss-1s <95505913+Boss-1s@users.noreply.github.com>
2026-07-21 21:12:34 +0000

    release: version update [skip-ci]
    This commit is standard release procedure and was automatically commited as part of a release. This commit was created and pushed by automation.

--- src/key_multivalue_storage/__init__.py
+++ src/key_multivalue_storage/__init__.py
@@ -36,8 +36,8 @@ from .delete import Delete
 from .utils import exceptions
 from .utils import warnings as kms_warnings
 
-__version__ = "v1.3.0.20260703a4"
-__version_internal__ = "kms-v1.3.0a4/2026.07.03"
+__version__ = "v1.3.0.20260721b0"
+__version_internal__ = "kms-v1.3.0beta0/2026.07.21"
 __author__ = "Boss_1s"
 __license__ = "GPLv2"
 
