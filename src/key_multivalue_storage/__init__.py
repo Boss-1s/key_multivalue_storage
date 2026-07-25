f090cf3c4456d28120ef719a79b597d4a412b3bc
Boss-1s <95505913+Boss-1s@users.noreply.github.com>
2026-07-25 02:31:09 +0000

    [skip ci] release: version update
    This commit is standard release procedure and was automatically commited as part of a release. This commit was created and pushed by automation.

--- src/key_multivalue_storage/__init__.py
+++ src/key_multivalue_storage/__init__.py
@@ -1,5 +1,5 @@
-__version__ = "v1.3.0.20260723b1.post0"
-__version_internal__ = "kms-v1.3.0b1/2026.07.23a"
+__version__ = "v1.2.5.20260724"
+__version_internal__ = "kms-v1.2.5/2026.07.24"
 __author__ = "Boss_1s"
 __email__ = "95505913+Boss-1s@users.noreply.github.com"
 __license__ = "GPLv2"
