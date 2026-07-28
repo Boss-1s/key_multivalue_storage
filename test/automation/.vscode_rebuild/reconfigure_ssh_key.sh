f37212a2b68137be9ee70307d685598cfaec0607
Boss-1s <95505913+Boss-1s@users.noreply.github.com>
2026-07-28 01:04:06 +0000

    fix: forgot line `git remote set-url`

--- test/automation/.vscode_rebuild/reconfigure_ssh_key.sh
+++ test/automation/.vscode_rebuild/reconfigure_ssh_key.sh
@@ -2,6 +2,8 @@
 
 bash .github/workflows/src/ssh.sh
 
+git remote set-url origin git@github.com:Boss-1s/key_multivalue_storage.git
+
 git config --global core.sshCommand "ssh -i ~/.ssh/id_ed25519"
 
 echo ''
