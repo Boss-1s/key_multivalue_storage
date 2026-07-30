df40c5bf3d93a07b72278b40ca731bacc57541b2
Boss_1s <95505913+Boss-1s@users.noreply.github.com>
2026-07-30 16:01:42 -0500

    feat: new test `test-exceptions`
    Signed-off-by: Boss_1s <95505913+Boss-1s@users.noreply.github.com>

---
+++ test/test-exceptions.py
@@ -0,0 +1,31 @@
+import key_multivalue_storage as kms
+from rich.traceback import install
+
+install(show_locals=True)
+
+print("Begin custom warnings/exceptions test.")
+
+assert kms.AddtionFailureWarning
+assert kms.SubtractionFailureWarning
+assert kms.DeleteWarning
+assert kms.CastWarning
+
+try:
+  raise kms.KeyNotFoundError
+except kms.KeyNotFoundError:
+  pass
+except Exception as e:
+  raise AssertionError(e)
+else:
+  raise AssertionError
+
+try:
+  raise kms.NoInstantiationError
+except kms.NoInstantiationError:
+  pass
+except Exception as e:
+  raise AssertionError(e)
+else:
+  raise AssertionError
+
+print("Test complete.")
