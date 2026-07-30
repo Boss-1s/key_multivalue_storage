27a223475b2ddff312fc7c862229c961da310d61
Boss_1s <95505913+Boss-1s@users.noreply.github.com>
2026-07-30 16:06:29 -0500

    feat: add example scenarios to exception docstrings
    Signed-off-by: Boss_1s <95505913+Boss-1s@users.noreply.github.com>

--- src/key_multivalue_storage/utils/exceptions.py
+++ src/key_multivalue_storage/utils/exceptions.py
@@ -11,7 +11,12 @@ from typing import Any
 
 #pylint: disable=too-many-ancestors
 class KeyNotFoundError(KeyError):
-    """Custom exception raised when a key is not found."""
+    """
+    Custom exception raised when a key is not found.
+    
+    Example: if attempting to search for a nonexistent key with
+    `Storage.Load.by_key`, this would be raised.
+    """
     def __init__(self, file: str, mkey: Any, message="") -> None:
         self.mkey = mkey
         self.file = file
@@ -25,4 +30,7 @@ class NoInstantiationError(TypeError):
     """
     Custom exception raised when attempting to instantiate a
     non-instantiable class.
+
+    Example: if attempting to instantiate a helper class like `Load`,
+    this would be raised.
     """
