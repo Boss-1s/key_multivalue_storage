24582ffb05b73d5319032e4f51f852ee91bbbbc6
Boss-1s <95505913+Boss-1s@users.noreply.github.com>
2026-08-01 15:33:45 +0000

    chore: module docstring
    Signed-off-by: GitHub <noreply@github.com>

--- test/test-exceptions.py
+++ test/test-exceptions.py
@@ -1,5 +1,12 @@
-import key_multivalue_storage as kms
+#pylint: disable=line-too-long
+"""
+test-exceptions.py - a test file for KMS exceptions and warnings.
+
+Compatible versions for this test file: >=kms-v1.3.0a0/2026.05.23
+"""
+
 from rich.traceback import install
+import key_multivalue_storage as kms
 
 install(show_locals=True)
 
@@ -11,21 +18,17 @@ assert kms.DeleteWarning
 assert kms.CastWarning
 
 try:
-  raise kms.KeyNotFoundError
+    raise kms.KeyNotFoundError("Test KeyNotFoundError", "This is a test for KeyNotFoundError.")
 except kms.KeyNotFoundError:
-  pass
+    pass
 except Exception as e:
-  raise AssertionError(e)
-else:
-  raise AssertionError
+    raise AssertionError(e) from e
 
 try:
-  raise kms.NoInstantiationError
+    raise kms.NoInstantiationError
 except kms.NoInstantiationError:
-  pass
+    pass
 except Exception as e:
-  raise AssertionError(e)
-else:
-  raise AssertionError
+    raise AssertionError(e) from e
 
 print("Test complete.")
