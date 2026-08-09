03564a292059223f494d5567f7fe45eda5ec4b10
Boss-1s <95505913+Boss-1s@users.noreply.github.com>
2026-08-09 23:07:50 +0000

    fix(tests): add fix-76 to test payload
    Signed-off-by: Boss-1s <95505913+Boss-1s@users.noreply.github.com>

--- test/__main__.py
+++ test/__main__.py
@@ -34,7 +34,8 @@ def main(c: Console) -> None:
             "test/test-exceptions.py",
             "test/test-fix-26-and-27.py",
             "test/test-fix-14.py",
-            "test/test-fix-67.py"
+            "test/test-fix-67.py",
+            "test/test-fix-76.py"
         ]
 
         for test_file in tests:
