d3d937584cfee7fa0b2d158fe18906f92ae7f0df
Boss-1s <95505913+Boss-1s@users.noreply.github.com>
2026-08-09 03:21:52 +0000

    fix: ensure vscode.sh gets reset after execution for security
    Signed-off-by: Boss-1s <95505913+Boss-1s@users.noreply.github.com>

--- vscode.sh
+++ vscode.sh
@@ -9,6 +9,8 @@ export SSH_PRIVATE_KEY="" # Enter your private key here
 python test reset_env "$SSH_PRIVATE_KEY" "user.name" "user.email" || true
 python test reset_env "$SSH_PRIVATE_KEY" "user.name" "user.email" #run twice bc python-enviroment will KeyboardInterrupt
 
-gh auth login
+gh auth login --scopes "repo,workflow,write:discussion,admin:repo_hook,admin:org,admin:public_key,admin:org_hook,user,project,gist,read:packages,write:packages,delete:packages,codespace"
 
 echo "You're all set! You can now develop for kms in VSCode!"
+
+git restore vscode.sh
