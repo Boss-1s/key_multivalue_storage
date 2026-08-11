9c099393d47bc240f4b3dedfa7b0b48a5ec4386d
Boss-1s <95505913+Boss-1s@users.noreply.github.com>
2026-08-11 11:47:05 +0000

    fix(tests): remove legacy test-general test
    Signed-off-by: Boss-1s <95505913+Boss-1s@users.noreply.github.com>

--- test/__main__.py
+++ test/__main__.py
@@ -45,7 +45,6 @@ def main(c: Console) -> None:
         item in ["a", "all"] for item in [item.lower() for item in sys.argv]
     ):
         tests = [
-            "test/test-general.py",
             "test/test-storage.py",
             "test/test-load.py",
             "test/test-edit.py",
