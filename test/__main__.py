9fca1e480512c88ef631725eb6d23b6080b4102b
Boss-1s <95505913+Boss-1s@users.noreply.github.com>
2026-08-11 03:26:11 +0000

    refactor(test): `test/__main__.py`
    ## Current coverage status
    - Without legacy `test-general`: 66% (~30% improvment)
    - With legacy `test-general`: 79% (13% improvment)
    
    Signed-off-by: Boss-1s <95505913+Boss-1s@users.noreply.github.com>

--- test/__main__.py
+++ test/__main__.py
@@ -16,10 +16,27 @@ import sys
 import time
 import argparse # TODO in kms-tester-semver0.1.0: better argument parsing
 import subprocess
-import traceback
 import warnings
 from rich.console import Console
 from rich.traceback import install
+
+def _run_tests(c: Console, tests: list[str]) -> None:
+    for test_file in tests:
+        try:
+            c.print(f"[blue]Running: {test_file}[/]")
+            exec(open(test_file).read(), globals())
+        except Exception as e:
+            print(f"❌ [red b]Error in {test_file}: {e}[/]")
+            c.print_exception(show_locals=True)
+            c.print(
+                "[b][blue]Hint:[/b] hit Ctrl+Z/Ctrl+C to examine the issue more carefully[/]"
+            )
+            time.sleep(1.9)
+
+        c.print("-" * 30)
+        time.sleep(0.1)
+
+    c.print("[green b]All tests complete![/]")
 
 def main(c: Console) -> None:
     """Main method for testing via command line"""
@@ -28,7 +45,7 @@ def main(c: Console) -> None:
         item in ["a", "all"] for item in [item.lower() for item in sys.argv]
     ):
         tests = [
-            # "test/test-general.py",
+            "test/test-general.py",
             "test/test-storage.py",
             "test/test-load.py",
             "test/test-edit.py",
@@ -42,110 +59,102 @@ def main(c: Console) -> None:
             "test/test-fix-80.py"
         ]
 
-        for test_file in tests:
+        _run_tests(c, tests)
+        return
+
+    match sys.argv[1].lower():
+        case "general":
             try:
-                c.print(f"[blue]Running: {test_file}[/]")
-                exec(open(test_file).read(), globals())
-            except Exception as e:
-                print(f"❌ [red b]Error in {test_file}: {e}[/]")
-                c.print_exception(show_locals=True)
-                time.sleep(1.9)
+                if sys.argv[2].lower() == '1.3':
+                    tests = [
+                        "test/test-storage.py",
+                        "test/test-load.py",
+                        "test/test-edit.py",
+                        "test/test-delete.py"
+                    ]
 
-            c.print("-" * 30)
-            time.sleep(0.1)
+                    _run_tests(c, tests)
+            except IndexError:
+                warnings.warn("You are using an OLD version of test-general, "+
+                            "sepcifically the one targetd for kms-semver1.2.x.\n"+
+                            "To run the new tests, run `python test/ general 1.3`.")
+                _run_tests(c, ["test/test-general.py"])
+        case "storage":
+            _run_tests(c, ["test/test-storage.py"])
+        case "load":
+            _run_tests(c, ["test/test-load.py"])
+        case "edit":
+            _run_tests(c, ["test/test-edit.py"])
+        case "delete":
+            _run_tests(c, ["test/test-delete.py"])
+        case "meta":
+            _run_tests(c, ["test/test-meta.py"])
+        case "exceptions" | "warnings":
+            _run_tests(c, ["test/test-exceptions.py"])
+        case "diff":
+            try:
+                os.environ["OLDTAG"] = sys.argv[2]
+            except IndexError:
+                warnings.warn("No argument OLDTAG provided. Falling back to script default...")
 
-        c.print("[green b]All tests complete![/]")
-        return
-    if sys.argv[1].lower() == "general":
-        warnings.warn("You are using an OLD version of test-general, "+
-                      "sepcifically the one targetd for kms-semver1.2.x.")
-        exec(open("test/test-general.py").read(), globals())
-        return
-    if sys.argv[1].lower() == "storage":
-        exec(open("test/test-storage.py").read(), globals())
-        return
-    if sys.argv[1].lower() == "load":
-        exec(open("test/test-load.py").read(), globals())
-        return
-    if sys.argv[1].lower() == "edit":
-        exec(open("test/test-edit.py").read(), globals())
-        return
-    if sys.argv[1].lower() == "delete":
-        exec(open("test/test-delete.py").read(), globals())
-        return
-    if sys.argv[1].lower() == "meta":
-        exec(open("test/test-meta.py").read(), globals())
-        return
-    if sys.argv[1].lower() in ["exceptions", "warnings"]:
-        exec(open("test/test-exceptions.py").read(), globals())
-        return
-    if sys.argv[1].lower() == "diff":
-        try:
-            os.environ["OLDTAG"] = sys.argv[2]
-        except IndexError:
-            warnings.warn("No argument OLDTAG provided. Falling back to script default...")
+            if os.environ.get("diff_py_tworef"):
+                del os.environ["diff_py_tworef"]
 
-        if os.environ.get("diff_py_tworef"): del os.environ["diff_py_tworef"]
+            c.print("[green bold]Finding breaking changes![/]")
+            _run_tests(c, ["test/diff.py"])
+        case "diff2":
+            try:
+                os.environ["OLDTAG"] = sys.argv[2]
+            except IndexError:
+                warnings.warn("No argument OLDTAG provided. Falling back to script default...")
 
-        c.print("[green bold]Finding breaking changes![/]")
-        exec(open("test/diff.py").read(), globals())
-        return
-    if sys.argv[1].lower() == "diff2":
-        try:
-            os.environ["OLDTAG"] = sys.argv[2]
-        except IndexError:
-            warnings.warn("No argument OLDTAG provided. Falling back to script default...")
+            try:
+                os.environ["NEWTAG"] = sys.argv[3]
+            except IndexError:
+                warnings.warn("No argument NEWTAG provided. Falling back to script default...")
 
-        try:
-            os.environ["NEWTAG"] = sys.argv[3]
-        except IndexError:
-            warnings.warn("No argument NEWTAG provided. Falling back to script default...")
+            os.environ["diff_py_tworef"] = '1'
+            c.print("[green bold]Finding breaking changes![/]")
+            _run_tests(c, ["test/diff.py"])
+        case "help_shortcut":
+            exec("import key_multivalue_storage as kms; kms.help()", globals())
+        case "reset_env":
+            try:
+                sys.argv[2]
+            except IndexError as e:
+                raise ValueError(
+                    "Please provide a SSH private key to use."
+                ) from e
 
-        os.environ["diff_py_tworef"] = '1'
-        c.print("[green bold]Finding breaking changes![/]")
-        exec(open("test/diff.py").read(), globals())
-        return
-    if sys.argv[1].lower() == "help_shortcut":
-        exec("import key_multivalue_storage as kms; kms.help()", globals())
-        return
-    if sys.argv[1].lower() == "reset_env":
-        try:
-            sys.argv[2]
-        except IndexError as e:
+            try:
+                sys.argv[3]
+            except IndexError as e:
+                raise ValueError(
+                    "Please provide a username for SSH-signed commits."
+                ) from e
+
+            try:
+                sys.argv[4]
+            except IndexError as e:
+                raise ValueError(
+                    "Please provide an email for SSH-signed commits."
+                ) from e
+
+            try:
+                clearall = bool(sys.argv[5])
+            except IndexError as e:
+                clearall = False
+
+            os.environ["SSH_PRIVATE_KEY"] = sys.argv[2]
+            os.environ["SSH_USER"] = sys.argv[3]
+            os.environ["SSH_EMAIL"] = sys.argv[4]
+            os.environ["reconfig_ssh_key_clearall"] = str(int(clearall))
+            subprocess.run(["python", "test/automation/.vscode_rebuild"], check=True)
+        case _:
             raise ValueError(
-                "Please provide a SSH private key to use."
-            ) from e
-
-        try:
-            sys.argv[3]
-        except IndexError as e:
-            raise ValueError(
-                "Please provide a username for SSH-signed commits."
-            ) from e
-
-        try:
-            sys.argv[4]
-        except IndexError as e:
-            raise ValueError(
-                "Please provide an email for SSH-signed commits."
-            ) from e
-
-        try:
-            clearall = bool(sys.argv[5])
-        except IndexError as e:
-            clearall = False
-
-        os.environ["SSH_PRIVATE_KEY"] = sys.argv[2]
-        os.environ["SSH_USER"] = sys.argv[3]
-        os.environ["SSH_EMAIL"] = sys.argv[4]
-        os.environ["reconfig_ssh_key_clearall"] = str(int(clearall))
-        subprocess.run(["python", "test/automation/.vscode_rebuild"], check=True)
-        return
-
-    raise ValueError(
-        "Invalid argument. Available arguments: a, all, general, meta, diff, "+
-        "exceptions, warnings, help_shortcut, reset_env"
-    )
+                "Invalid argument. Available arguments: a, all, general, meta, diff, "+
+                "exceptions, warnings, help_shortcut, reset_env"
+            )
 
 if __name__ == "__main__":
     console = Console()
