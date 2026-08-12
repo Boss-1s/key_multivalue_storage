3f731eb980789e1326506143c26cf4e9447b1605
Boss-1s <95505913+Boss-1s@users.noreply.github.com>
2026-08-12 13:03:00 +0000

    feat(automations): auto-change version for sonar-project.properties
    Signed-off-by: Boss-1s <95505913+Boss-1s@users.noreply.github.com>

--- test/automation/release.py
+++ test/automation/release.py
@@ -11,6 +11,7 @@ except NameError:
     repo_root = os.environ.get("GITHUB_WORKSPACE", os.getcwd())
 pyproject_path = os.path.join(repo_root, "pyproject.toml")
 init_py_path = os.path.join(repo_root, "src/key_multivalue_storage/__init__.py")
+sonar_path = os.path.join(repo_root, 'sonar-project.properties')
 nightly = os.environ.get("NIGHTLY")
 newv = os.environ.get("NVERSION")
 newv_name = os.environ.get("RELEASENVERSION")
@@ -71,5 +72,26 @@ with open(init_py_path, "r+", encoding='utf-8') as f:
 
 with open(init_py_path, "r", encoding='utf-8') as f:
     print("New __init__.py file:\033[32m")
+    print(f.read())
+    print("\033[0m")
+
+with open(sonar_path, "r+", encoding='utf-8') as f:
+    pyproject = f.readlines()
+
+    f.seek(0)
+
+    for line in pyproject:
+        if line.startswith('sonar.projectVersion='):
+            print(f"::notice:: release.py: replacing line '{line.replace('\n', '')}'")
+            newline = f'sonar.projectVersion={newv}\n'
+            f.write(newline)
+            print(f"::notice:: release.py: line is now '{newline.replace('\n', '')}'")
+        else:
+            f.write(line)
+
+    f.truncate()
+
+with open(sonar_path, "r", encoding='utf-8') as f:
+    print("New sonar-project.properties file:\033[32m")
     print(f.read())
     print("\033[0m")
