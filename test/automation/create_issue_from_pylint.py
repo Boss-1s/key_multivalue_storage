2a7212808efef1aa4b7d16a35f3dc008ba67714f
Boss-1s <95505913+Boss-1s@users.noreply.github.com>
2026-08-22 03:44:46 +0000

    feat(tests): automatic issue creation! part 1
    Signed-off-by: GitHub <noreply@github.com>

--- test/automation/create_issue_from_pylint.py
+++ test/automation/create_issue_from_pylint.py
@@ -6,16 +6,48 @@ def main():
     with open('pylint_report.json', 'r') as f:
         errors = json.load(f)
 
+    subprocess.run('gh issue list --json number,title,body > issues.json',
+                   shell=True,
+                   check=True,
+                   capture_output=True)
+
+    with open('issues.json', 'r') as f:
+        existing_issues = json.load(f)
+
     # 2. Iterate through errors and create issues
-    for error in errors:
-        msg_id = error['message-id']
-        line = error['line']
-        path = error['path']
-        message = error['message']
+    for i in range(len(errors['messages'])):
+        error = errors['messages'][i]
+        if error['type'] != 'error':
+            continue  # Skip non-error messages
+
+        msg_id: str = error['messageId']
+        line: int = error['line']
+        path: str = error['path']
+        message: str = error['message']
 
         # Construct the GitHub Issue title and body
-        title = f"[{msg_id}] Lint error in {path} at line {line}"
-        body = f"**File:** {path}\n**Line:** {line}\n**Error ID:** {msg_id}\n\n**Description:**\n{message}"
+        title = f"fatal: pylint error {msg_id}"
+        body = f"""
+**File:** [{path}](https://github.com/key_multivalue_storage/tree/semver1.4.x/{path})
+**Line:** {line}
+**Error ID:** {msg_id}
+
+**Description:**
+{message}
+"""
+
+        # Check if the issue already exists
+        issue_exists = False
+        issue_id = None
+        for issue in existing_issues:
+            issue_exists = title in issue['title'] and body in issue['body']
+            if issue_exists:
+                issue_id = issue['number']
+                break
+
+        if issue_exists:
+            print(f"Issue already exists for {title}, with id {issue_id}, skipping...")
+            continue
 
         # 3. Use the `gh` CLI to create the issue
         subprocess.run([
@@ -23,7 +55,7 @@ def main():
             '--title', title,
             '--body', body,
             '--label', 'bug,pylint,Needs Triage'
-            ], check=True
+            ], check=True, capture_output=True
         )
 
     print("Successfully processed all Pylint errors into GitHub issues!")
