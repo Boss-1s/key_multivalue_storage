3de6664273588b2601d8a2c9355d8e084bf47c87
Boss_1s <95505913+Boss-1s@users.noreply.github.com>
2026-09-22 15:38:12 +0000

    chore(release): fix up post-release script
    - Better diffing
    - Test mode
    - Verbose or Silent Mode
    - Rich prints
    
    Signed-off-by: GitHub <noreply@github.com>

--- test/automation/post-release.py
+++ test/automation/post-release.py
@@ -4,6 +4,9 @@ Module to dynamically bump past the current version into a dev version.
 """
 import os
 import argparse
+import subprocess
+import builtins
+from rich.console import Console
 from rich.traceback import install
 
 def main():
@@ -21,11 +24,28 @@ def main():
                         type=int,
                         default=4,
                         help="Depth of the directory to find the repo root")
+    parser.add_argument("--test",
+                        action="store_true",
+                        default=False,
+                        help="Run in test mode (dry-run)")
+    parser.add_argument("--verbose",
+                        action="store_true",
+                        default=False,
+                        help="Enable verbose output")
     args = parser.parse_args()
 
-    newv = args.version
-    newv_name = args.name
-    dir_depth = args.dir_depth
+    newv: str = args.version
+    newv_name: str = args.name
+    og_v: str = ''
+    dir_depth: int = args.dir_depth
+    test_mode: bool = args.test
+    verbose: bool = args.verbose
+
+    console = Console(quiet=not verbose, force_terminal=True)
+
+    print = console.print
+
+    install(console=console, show_locals=verbose)
 
     try:
         repo_root = os.environ.get("GITHUB_WORKSPACE",
@@ -61,10 +81,8 @@ def main():
 
             f.truncate()
 
-        with open(pyproject_path, "r", encoding="utf-8") as f:
-            print("New pyproject.toml file:\033[32m")
-            print(f.read())
-            print("\033[0m")
+        print("[b blue]Diff pyproject.toml file:[/]")
+        builtins.print(subprocess.run(["git", "--no-pager", "diff", "--color", pyproject_path], check=True, capture_output=True).stdout.decode("utf-8"))
 
         with open(init_py_path, "r+", encoding='utf-8') as f:
             pyproject = f.readlines()
@@ -73,7 +91,8 @@ def main():
 
             for line in pyproject:
                 if line.startswith('__version__ = '):
-                    print(f"::notice:: release.py: replacing line '{line.replace('\n', '')}'")
+                    og_v = line.replace('\n', '').replace('__version__ = ', '').replace('"', '')
+                    print(f"::notice:: release.py: replacing line '{og_v}'")
                     newline = f'__version__ = "{newv}"\n'
                     f.write(newline)
                     print(f"::notice:: release.py: line is now '{newline.replace('\n', '')}'")
@@ -87,13 +106,43 @@ def main():
 
             f.truncate()
 
-        with open(init_py_path, "r", encoding='utf-8') as f:
-            print("New __init__.py file:\033[32m")
-            print(f.read())
-            print("\033[0m")
+        print("[b blue]Diff __init__.py file:[/]")
+        builtins.print(subprocess.run(["git", "--no-pager", "diff", "--color", init_py_path], check=True, capture_output=True).stdout.decode("utf-8"))
     except FileNotFoundError as e:
         raise TypeError(str(e) + " -- Is your directory depth set correctly?") from e
+    except subprocess.CalledProcessError as e:
+        raise SyntaxError(
+            f"::error:: Internal error while attempting to diff files: {str(e)}"
+        ) from e
+
+    try:
+        subprocess.run(["git", "add", pyproject_path, init_py_path],
+                       check=True,
+                       capture_output=True)
+        subprocess.run(["git", "status", "--porcelain"], check=True, capture_output=True)
+        subprocess.run(["git", "commit", "-m", f"release: Post {og_v}",
+                        "--dry-run" if test_mode else ''],
+                    check=True,
+                    capture_output=True)
+        subprocess.run(["git", "push", "--dry-run" if test_mode else ''],
+                    check=True,
+                    capture_output=True)
+    except Exception as e:
+        raise SyntaxError(
+            f"::error:: Internal error while attempting to commit: {str(e)}"
+        ) from e
+    finally:
+        if test_mode:
+            print("::notice:: release.py: Test mode enabled, nothing was commited or pushed.")
+            subprocess.run(["git", "restore", "--staged", pyproject_path, init_py_path],
+                           check=True,
+                           capture_output=True)
+            subprocess.run(["git", "restore", pyproject_path, init_py_path],
+                           check=True,
+                           capture_output=True)
+
+    print("::notice:: release.py: Finished post-release version bumping.")
+
 
 if __name__ == "__main__":
-    install(show_locals=True)
     main()
