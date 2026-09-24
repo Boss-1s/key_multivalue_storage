8a2cc06cbd700727e4b9da2015a4256c24e695c7
Boss_1s <95505913+Boss-1s@users.noreply.github.com>
2026-09-24 09:42:50 -0500

    feat(kms.storage): add `update()`
    Signed-off-by: Boss_1s <95505913+Boss-1s@users.noreply.github.com>

--- src/key_multivalue_storage/storage.py
+++ src/key_multivalue_storage/storage.py
@@ -406,6 +406,28 @@ class Storage(metaclass=meta._StorageMeta):
         """
         return {self.key: None}.keys()
 
+    def update(self, other=(), /, **kwds):
+        '''
+        D.update([E, ]**F) -> None.  Update D from mapping/iterable E and F.
+            If E present and has a .keys() method, does:
+                for k in E.keys(): D[k] = E[k]
+            If E present and lacks .keys() method, does:
+                for (k, v) in E: D[k] = v
+            In either case, this is followed by:
+                for k, v in F.items(): D[k] = v
+        '''
+        if isinstance(other, Mapping):
+            for key in other:
+                self[key] = other[key]
+        elif hasattr(other, "keys"):
+            for key in other.keys():
+                self[key] = other[key]
+        else:
+            for key, value in other:
+                self[key] = value
+        for key, value in kwds.items():
+            self[key] = value
+
     # --- #
 
     _default_valueerror_msg: str = "Both instances must have the same top level key"
